eb614c7664f1996f368495940fa0fd016c24641c
Sam McCants <samuel@hibirdi.com>
2019-07-03 10:58:11 +0100

    missing semicolon

--- CatalogAPI/GetCatalogForPoint.js
+++ CatalogAPI/GetCatalogForPoint.js
@@ -4,7 +4,7 @@ const xhr = new XMLHttpRequest();
 const lat = 55.862261;
 const lng = -4.250959;
 const zoom = 16;
-const token = "ADD YOUR TOKEN HERE"
+const token = "ADD YOUR TOKEN HERE";
 const url = "https://catalog-api.hibirdi.com/catalog/lat/" + lat +
             "/lng/" + lng +
             "/zoom/" + zoom +
