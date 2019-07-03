78246f5fb0892b9f12cbeb970934b93b77ec9a90
Sam McCants <samuel@hibirdi.com>
2019-07-03 10:45:59 +0100

    separated values into variables & formatted

--- CatalogAPI/GetCatalogForPoint.js
+++ CatalogAPI/GetCatalogForPoint.js
@@ -1,7 +1,14 @@
 let data = null;
 const xhr = new XMLHttpRequest();
+
+const lat = 55.862261;
+const lng = -4.250959;
+const zoom = 16;
 const token = "ADD YOUR TOKEN HERE"
-const url = "https://catalog-api.hibirdi.com/catalog/lat/55.862261/lng/-4.250959/zoom/16/token/" + token
+const url = "https://catalog-api.hibirdi.com/catalog/lat/" + lat +
+            "/lng/" + lng +
+            "/zoom/" + zoom +
+            "/token/" + token;
 
 xhr.addEventListener("readystatechange", function () {
   if (this.readyState === 4) {
