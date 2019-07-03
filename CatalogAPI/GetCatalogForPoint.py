62b26b7d7057c47fe1cad0777199fa7af30dce4a
Sam McCants <samuel@hibirdi.com>
2019-07-03 10:46:04 +0100

    separated values into variables & formatted

--- CatalogAPI/GetCatalogForPoint.py
+++ CatalogAPI/GetCatalogForPoint.py
@@ -1,7 +1,12 @@
 import requests
 
+lat = 55.862261
+lng = -4.250959
+zoom = 16
 token = "ADD YOUR TOKEN HERE"
-url = "https://catalog-api.hibirdi.com/catalog/lat/55.862261/lng/-4.250959/zoom/16/token/" + token
+
+url = ("https://catalog-api.hibirdi.com/catalog/lat/{}/lng/{}/zoom/{}/token/{}"
+       .format(lat, lng, zoom, token))
 
 response = requests.request("GET", url)
 
