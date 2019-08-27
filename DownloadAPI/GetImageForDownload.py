60064de4e1e9500ca3955006b574e28bc3eb9994
Sam McCants <samuel@hibirdi.com>
2019-08-27 14:39:12 +0100

    print status code instead of trying to print an image / use get() for get request

--- DownloadAPI/GetImageForDownload.py
+++ DownloadAPI/GetImageForDownload.py
@@ -9,6 +9,6 @@ token = "ADD YOUR TOKEN HERE"
 url = ("https://download-api.hibirdi.com/download/lat/{}/lng/{}/zoom/{}/id/{}?token={}"
        .format(lat, lng, zoom, imageId, token))
 
-response = requests.request("GET", url)
+response = requests.get(url)
 
-print(response.text)
+print(response.status_code)
