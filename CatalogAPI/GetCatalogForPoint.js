9e1c5e9e61bf6b24eaef7e227927b25452d494ce
Sam McCants <samuel@hibirdi.com>
2019-11-27 16:20:12 +0000

    combined examples for the combined endpoints

--- CatalogAPI/GetCatalogForPoint.js
+++ CatalogAPI/GetCatalogForPoint.js
@@ -5,10 +5,18 @@ const lat = 55.862261;
 const lng = -4.250959;
 const zoom = 16;
 const token = "ADD YOUR TOKEN HERE";
+const blackAndWhiteAllowed = false;
+const cloudCover = 20;
+const startDate = 1535371200000;
+const providers = "BLUESKY,DIGITALGLOBE";
 const url = "https://catalog-api.hibirdi.com/catalog/lat/" + lat +
             "/lng/" + lng +
             "/zoom/" + zoom +
-            "/token/" + token;
+            "/token/" + token
+            "?blackAndWhiteAllowed=" + blackAndWhiteAllowed +
+            "&cloudCover=" + cloudCover +
+            "&startDate=" + startDate +
+            "&providers=" + providers;
 
 xhr.addEventListener("readystatechange", function () {
   if (this.readyState === 4) {
--- CatalogAPI/GetCatalogForPoint.py
+++ CatalogAPI/GetCatalogForPoint.py
@@ -4,9 +4,13 @@ lat = 55.862261
 lng = -4.250959
 zoom = 16
 token = "ADD YOUR TOKEN HERE"
+blackAndWhiteAllowed = False
+cloudCover = 20
+startDate = 1535371200000
+providers = "BLUESKY,DIGITALGLOBE"
 
-url = ("https://catalog-api.hibirdi.com/catalog/lat/{}/lng/{}/zoom/{}/token/{}"
-       .format(lat, lng, zoom, token))
+url = ("https://catalog-api.hibirdi.com/catalog/lat/{}/lng/{}/zoom/{}/token/{}?blackAndWhiteAllowed={}&cloudCover={}&startDate={}&providers={}"
+       .format(lat, lng, zoom, token, blackAndWhiteAllowed, cloudCover, startDate, providers))
 
 response = requests.request("GET", url)
 
